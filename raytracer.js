a215062f63a1127d8da500f2234f6be5f43d6d10
RSYashwanth <yashwanthrs1104@gmail.com>
2024-08-03 23:53:44 -0500

    Cleaned Raytracer.js

--- raytracer.js
+++ raytracer.js
@@ -1,58 +1,58 @@
-import { VectorUtils, Scene, Ray, Material, Sphere, Camera } from "./utility.js";
+import { Camera, Ray, Scene, Sphere, VectorUtils } from "./utility.js";
 import { createControls } from "./ui.js";
 
-let canvas = document.getElementById("view");
-let context = canvas.getContext("2d")
+const canvas = document.getElementById("view");
+const context = canvas.getContext("2d");
 
-let width = canvas.width;
-let height = canvas.height;
+const width = canvas.width;
+const height = canvas.height;
 
-let scene = new Scene();
+const scene = new Scene();
 
 // Listener to add more spheres when add objects button is clicked
-let addBtn = document.getElementById("add");
+const addBtn = document.getElementById("add");
 addBtn.onclick = () => {
-    let sphere = new Sphere();
+    const sphere = new Sphere();
     createControls(sphere);
     scene.add(sphere);
     addBtn.blur();
-}
-
+};
 
 // Create camera
-let camera = new Camera({fov: 40 * width / 50});
+const camera = new Camera({ fov: 40 * width / 50 });
 
 // Define number of samples to be rendered
 let samples = 100;
-let sampleRange = document.getElementById("sample");
-let sampleLabel = document.getElementById("sample-label");
+const sampleRange = document.getElementById("sample");
+const sampleLabel = document.getElementById("sample-label");
 sampleLabel.textContent = 100;
 sampleRange.onmousemove = () => {
     samples = sampleRange.value;
     sampleLabel.textContent = samples;
-}
-
+};
 
 // Recursive ray tracing function
 function trace(ray, bounce) {
-    let light = {
+    const light = {
         x: 0,
         y: 0,
-        z: 0
-    }
+        z: 0,
+    };
 
     // Base Case
     if (bounce <= 0) {
         return light;
-    }
-    // Recursive Case
-    else{
+    } // Recursive Case
+    else {
         // Find closest object that the ray intersected
         let collisionInfo = null;
         scene.getObjects().forEach((object) => {
-            let current = object.calcHit(ray);
-            if(current.intersect) {
-                if(collisionInfo == null || collisionInfo.distance >= current.distance) {
+            const current = object.calcHit(ray);
+            if (current.intersect) {
+                if (
+                    collisionInfo == null ||
+                    collisionInfo.distance >= current.distance
+                ) {
                     collisionInfo = current;
                 }
             }
@@ -64,33 +64,51 @@ function trace(ray, bounce) {
         }
 
         // Else calculate pixel color based on material properties and further light bounces
-        let emission = VectorUtils.scalarMult(collisionInfo.obj.material.emissiveColor, collisionInfo.obj.material.emissiveStrength);
-        let color = collisionInfo.obj.material.color;
+        const emission = VectorUtils.scalarMult(
+            collisionInfo.obj.material.emissiveColor,
+            collisionInfo.obj.material.emissiveStrength,
+        );
+        const color = collisionInfo.obj.material.color;
 
-        let incomingRay = calculateRayFrom(ray, collisionInfo)
+        const incomingRay = calculateRayFrom(ray, collisionInfo);
 
-        let incomingLight = trace(incomingRay, bounce - 1);
+        const incomingLight = trace(incomingRay, bounce - 1);
 
-        return VectorUtils.add(emission, VectorUtils.mult(color, incomingLight));
+        return VectorUtils.add(
+            emission,
+            VectorUtils.mult(color, incomingLight),
+        );
     }
 }
 
 // Calculate the reflected angle of the ray based on roughness and collision
 function calculateRayFrom(ray, collisionInfo) {
     // Calculate the exact reflection angle
-    let reflectDirection = VectorUtils.sub(ray.direction, VectorUtils.scalarMult(collisionInfo.normal, 2 * VectorUtils.dotProduct(ray.direction, collisionInfo.normal)));
+    let reflectDirection = VectorUtils.sub(
+        ray.direction,
+        VectorUtils.scalarMult(
+            collisionInfo.normal,
+            2 * VectorUtils.dotProduct(ray.direction, collisionInfo.normal),
+        ),
+    );
 
     // Offset this direction in proportion to roughness to account for glossy and rough surfaces
     reflectDirection = {
-        x: reflectDirection.x + (Math.random() * 2 - 1) * collisionInfo.obj.material.roughness / 2,
-        y: reflectDirection.y + (Math.random() * 2 - 1) * collisionInfo.obj.material.roughness / 2,
-        z: reflectDirection.z + (Math.random() * 2 - 1) * collisionInfo.obj.material.roughness / 2,
-    }
+        x: reflectDirection.x +
+            (Math.random() * 2 - 1) * collisionInfo.obj.material.roughness / 2,
+        y: reflectDirection.y +
+            (Math.random() * 2 - 1) * collisionInfo.obj.material.roughness / 2,
+        z: reflectDirection.z +
+            (Math.random() * 2 - 1) * collisionInfo.obj.material.roughness / 2,
+    };
 
     // If angle is greater than 90 with the normal, reverse the direction of the ray
-    let angleCheck = VectorUtils.dotProduct(reflectDirection, collisionInfo.normal);
+    const angleCheck = VectorUtils.dotProduct(
+        reflectDirection,
+        collisionInfo.normal,
+    );
     let s = 1;
-    if(angleCheck < 0) {
+    if (angleCheck < 0) {
         s = -1;
     }
     reflectDirection = VectorUtils.scalarMult(reflectDirection, s);
@@ -100,28 +118,33 @@ function calculateRayFrom(ray, collisionInfo) {
 
 // Calculate initial ray from the camera based on camera properties and pixel
 function calculateCameraRay(x, y, camera) {
-    let i = x - width / 2
-    let j = y - height / 2
-    return new Ray(camera.position, VectorUtils.rotateVector(VectorUtils.normalize({x: i, y: j, z: camera.fov}), camera.rotation));
+    const i = x - width / 2;
+    const j = y - height / 2;
+    return new Ray(
+        camera.position,
+        VectorUtils.rotateVector(
+            VectorUtils.normalize({ x: i, y: j, z: camera.fov }),
+            camera.rotation,
+        ),
+    );
 }
 
 // Render function to use the ray tracer
 function rayTraceRender() {
-    let imageData = context.createImageData(width, height);
-    let data = imageData.data;
-    for(let s = 1; s <= samples; s++) {
+    const imageData = context.createImageData(width, height);
+    const data = imageData.data;
+    for (let s = 1; s <= samples; s++) {
         for (let x = 0; x < width; x++) {
             for (let y = 0; y < height; y++) {
-                
-                let ray = calculateCameraRay(x, y, camera);
-                let color = trace(ray, 10);
+                const ray = calculateCameraRay(x, y, camera);
+                const color = trace(ray, 10);
 
-                let index = (y * width + x) * 4;
+                const index = (y * width + x) * 4;
 
-                data[index] += color.x / samples
-                data[index + 1] += color.y / samples
-                data[index + 2] += color.z / samples
-                data[index + 3] = 255
+                data[index] += color.x / samples;
+                data[index + 1] += color.y / samples;
+                data[index + 2] += color.z / samples;
+                data[index + 3] = 255;
             }
         }
         console.clear();
@@ -132,39 +155,40 @@ function rayTraceRender() {
 
 // Simpler trace function, used when modifying material properties to have a lower impact on performance
 function simpleTrace() {
-    let imageData = context.createImageData(width, height);
-    let data = imageData.data;
+    const imageData = context.createImageData(width, height);
+    const data = imageData.data;
 
     for (let x = 0; x < width; x++) {
         for (let y = 0; y < height; y++) {
-            let ray = calculateCameraRay(x, y, camera);
+            const ray = calculateCameraRay(x, y, camera);
 
             let collisionInfo = null;
             scene.getObjects().forEach((object) => {
-                let current = object.calcHit(ray);
-                if(current.intersect) {
-                    if(collisionInfo == null || collisionInfo.distance >= current.distance) {
+                const current = object.calcHit(ray);
+                if (current.intersect) {
+                    if (
+                        collisionInfo == null ||
+                        collisionInfo.distance >= current.distance
+                    ) {
                         collisionInfo = current;
                     }
                 }
             });
-        
-            if(collisionInfo == null) {
-
-            }
-            else {
-                // Only process one bounce of ray and color it with the objects color directly 
-                let color = collisionInfo.obj.material.color;
-                let red = color.x * 255;
-                let green = color.y * 255;
-                let blue = color.z * 255;
+            if (collisionInfo == null) {
+                continue;
+            } else {
+                // Only process one bounce of ray and color it with the objects color directly
+                const color = collisionInfo.obj.material.color;
+                const red = color.x * 255;
+                const green = color.y * 255;
+                const blue = color.z * 255;
 
                 const index = (y * width + x) * 4;
 
                 data[index] = red;
-                data[index + 1] = green;    
-                data[index + 2] = blue;  
-                data[index + 3] = 255;  
+                data[index + 1] = green;
+                data[index + 2] = blue;
+                data[index + 3] = 255;
             }
         }
     }
@@ -183,14 +207,13 @@ function drawFps() {
     lastFrameTime = currentTime;
 
     context.font = "14px Arial";
-    
-    if(count%5 == 0) {
+
+    if (count % 5 == 0) {
         context.fillStyle = "orange";
-        currentFps = Math.round(1000/deltaTime);
+        currentFps = Math.round(1000 / deltaTime);
         context.fillText(currentFps, 10, 20);
         count == 0;
-    }
-    else {
+    } else {
         context.fillStyle = "orange";
         context.fillText(Math.round(currentFps), 10, 20);
     }
@@ -199,10 +222,10 @@ function drawFps() {
 }
 
 // Listener to render the image and stop movement triggers from activating during rendering
-let renderBtn = document.getElementById("render");
-renderBtn.onclick =(e) => {
+const renderBtn = document.getElementById("render");
+renderBtn.onclick = (e) => {
     render = !render;
-    if(!render) {
+    if (!render) {
         requestAnimationFrame(update);
     }
     renderBtn.blur();
@@ -216,18 +239,14 @@ function update() {
     context.fillRect(0, 0, canvas.width, canvas.height);
 
     // If rendering, render and dont move or update frames
-    if(render) {
+    if (render) {
         rayTraceRender();
-    }
-    // Else use viewport mode rendering and move and draw frames and fps
+    } // Else use viewport mode rendering and move and draw frames and fps
     else {
         simpleTrace();
         camera.move();
-        
         drawFps();
-
         requestAnimationFrame(update);
     }
-
 }
 requestAnimationFrame(update);
